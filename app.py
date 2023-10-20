716e93b4c175a552847924e41d02d2f2d4357bba
amazingsufiyancode <133307239+amazingsufiyancode@users.noreply.github.com>
2023-10-20 07:28:41 +0000

    Added Jobs info

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$100,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'San Francisco',
+    'salary': '$120,000'
+  },
+  {
+    'id': 3,
+    'title': 'Software Engineer',
+    'location': 'Chicago'
+    
+  },
+  {
+    'id': 4,
+    'title': 'Data Analyst',
+    'location': 'Los Angeles',
+    'salary': '$90,000'
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         jobs = JOBS,
+                        comp_name = "Amazing")
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
 
 
 if __name__ == "__main__":
